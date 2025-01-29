9659f79fcf5d8df5e7a51555dbe10c5f1a43dc26
BSalemi <briandsalemi@gmail.com>
2025-01-29 06:10:16 -0500

    added Friend component with image and name of friends

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,5 +1,10 @@
 function Friend({ friend }) {
-  return <li>{friend.name}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+    </li>
+  );
 }
 
 export default Friend;
