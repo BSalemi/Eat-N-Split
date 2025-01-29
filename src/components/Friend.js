ae77a5ed6dd4ba2f099db5ccd8751c4cd425d43f
BSalemi <briandsalemi@gmail.com>
2025-01-29 06:15:30 -0500

    Added the balance owed to the Friend component

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -3,6 +3,17 @@ function Friend({ friend }) {
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {friend.balance}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}
+        </p>
+      )}
     </li>
   );
 }
