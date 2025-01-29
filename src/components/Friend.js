4cf0c323a4d85c6491b37628a01724e69bc80456
BSalemi <briandsalemi@gmail.com>
2025-01-29 06:26:00 -0500

    added balance rendering for when the balance is zero

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -6,14 +6,15 @@ function Friend({ friend }) {
 
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {friend.balance}
+          You owe {friend.name} {Math.abs(friend.balance)}
         </p>
       )}
       {friend.balance > 0 && (
         <p className="green">
-          {friend.name} owes you {friend.balance}
+          {friend.name} owes you {Math.abs(friend.balance)}
         </p>
       )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
     </li>
   );
 }
