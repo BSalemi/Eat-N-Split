1b74b819d6da45090a75a43c33a9e8894fe3dbb0
BSalemi <briandsalemi@gmail.com>
2025-01-30 07:20:36 -0500

    created AddFriendForm component

---
+++ src/components/AddFriendForm.js
@@ -0,0 +1,10 @@
+function AddFriendForm() {
+  return (
+    <form className="form-add-friend">
+      <label>👫 Friend name</label>
+      <input type="text" />
+    </form>
+  );
+}
+
+export default AddFriendForm;
