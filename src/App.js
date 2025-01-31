86a60efc953359b1149a2fd3d2788c4e75177720
BSalemi <briandsalemi@gmail.com>
2025-01-30 20:42:30 -0500

    created the SplitBillForm component and rendered it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import AddFriendForm from "./components/AddFriendForm";
+import Button from "./components/Button";
 import FriendsList from "./components/FriendsList";
+import SplitBillForm from "./components/SplitBillForm";
 
 function App() {
   return (
@@ -7,7 +9,9 @@ function App() {
       <div className="sidebar">
         <FriendsList />
         <AddFriendForm />
+        <Button>Add Friend</Button>
       </div>
+      <SplitBillForm />
     </div>
   );
 }
