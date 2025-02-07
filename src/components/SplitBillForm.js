d86623cd8d67f3a19b1396e43bd696484d57af48
BSalemi <briandsalemi@gmail.com>
2025-02-06 21:06:41 -0500

    added state and controlled elements to the input and select fields in the splitBillForm

--- src/components/SplitBillForm.js
+++ src/components/SplitBillForm.js
@@ -3,21 +3,40 @@ import { useState } from "react";
 function SplitBillForm({ selectedFriend }) {
   const [bill, setBill] = useState("");
   const [paidByUser, setPaidByUser] = useState("");
+  const [paidByFriend, setPaidByFriend] = useState("");
   const [whoIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
+      />
 
       <label>🧍‍♀️ Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        disabled={whoIsPaying === "user"}
+        value={paidByUser}
+        onChange={(e) => setPaidByUser(e.target.value)}
+      />
 
       <label>👫 {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input
+        type="text"
+        disabled={whoIsPaying === "you"}
+        value={paidByFriend}
+        onChange={(e) => setPaidByFriend(e.target.value)}
+      />
 
       <label>🤑 Who is paying the bill?</label>
-      <select>
+      <select
+        selected={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="you"> {selectedFriend.name} </option>
       </select>
