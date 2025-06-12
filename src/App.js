c8562e659b85d07ae4a2a11a45c16340e479fdc7
BSalemi <briandsalemi@gmail.com>
2025-06-12 16:24:15 -0400

    added key to the SplitBillForm component to clear the data out if the user switches friends while the form is open

--- src/App.js
+++ src/App.js
@@ -76,6 +76,7 @@ function App() {
         <SplitBillForm
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
